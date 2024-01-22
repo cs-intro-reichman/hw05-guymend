272a6f00082765638174b9c8b75f558e3aeee96c
guymend <guy.mendelson@post.runi.ac.il>
2024-01-22 16:05:58 +0200

    maybe now?

--- GameOfLife.java
+++ GameOfLife.java
@@ -166,10 +166,10 @@ public class GameOfLife {
 		//// Write your code here.
 		for(int i = 1; i < arr.length-1; i++){
 			for(int j = 1; j < arr[0].length-1; j++){
-				if(j == arr[0].length-2){
+				if((i == 1) && (j == 1)){
 					System.out.print(arr[i][j]);
 				}else{
-					System.out.print(arr[i][j]+"  ");
+					System.out.print("  "+arr[i][j]);
 				}
 			}
 			System.out.println("");
